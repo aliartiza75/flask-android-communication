069652f8b154dbb86737d248db04f9747482ce43
irti <iali@an10.io>
2018-08-11 23:34:23 +0500

    updated read me file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Flask Api & Anroid App
+
+This repository contains two folder:
+
+ - Secondlook [contains android application code]
+ - secondlook [contains flask REST Api code]
--- app.py
+++ app.py
@@ -6,16 +6,10 @@ app = Flask(__name__)
 tasks = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'title': u'Flask API',
+        'description': u'It is an rest api build in flask', 
         'done': False
     },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
 ]
 
 
